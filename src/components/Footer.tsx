91e815e9e060f00ca1f0ad279407d54131c2c575
Ken Yeck <ken.yeck@gmail.com>
2025-05-30 22:05:05 -0700

    Converted github/linkedin to use icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-4 text-center">
@@ -9,7 +11,7 @@ export default function Footer() {
           href="https://github.com/kyeckeq"
           className="mx-2 hover:text-gray-300"
         >
-          GitHub
+          <FaGithub className="inline mr-1" size="1.5em" />
         </a>
         <a
           target="_blank"
@@ -17,7 +19,7 @@ export default function Footer() {
           href="https://linkedin.com/in/kenyeck/"
           className="mx-2 hover:text-gray-300"
         >
-          LinkedIn
+          <FaLinkedin className="inline mr-1" size="1.5em" />
         </a>
       </div>
     </footer>
