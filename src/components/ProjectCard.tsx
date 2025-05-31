381a9f75ca24bbc2ffe4a1cd38bdcf56b538da03
Ken Yeck <ken.yeck@gmail.com>
2025-05-30 22:07:49 -0700

    Added descriptions and dimensions to Screenshot object

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
 
-export interface Project {
+interface Screenshot {
+  src: string;
+  desc?: string;
+  width?: number;
+  height?: number;
+}
+interface Project {
   title: string;
   description: string;
   technologies: string[];
-  screenshots: string[];
+  screenshots: Screenshot[];
 }
 
 interface ProjectCardProps {
@@ -15,25 +21,33 @@ export default function ProjectCard({ project }: ProjectCardProps) {
   return (
     <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-6 mb-6">
       <h2 className="text-2xl font-bold mb-2">{project.title}</h2>
-      <p className="text-gray-700 dark:text-gray-100 mb-4">{project.description}</p>
-      <div className="mb-4">
+      <p className="text-gray-700 dark:text-gray-100 mb-4">
+        {project.description}
+      </p>
+      <div className="mb-4 flex items-center">
         <h3 className="text-lg font-semibold">Technologies:</h3>
-        <ul className="list-disc list-inside">
+        <div className="text-lg text-gray-600 dark:text-gray-300 h-fit ml-2">
+          {project.technologies.join(', ')}
+        </div>
+        {/* <ul className="list-disc list-inside">
           {project.technologies.map((tech, index) => (
             <li key={index}>{tech}</li>
           ))}
-        </ul>
+        </ul> */}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {project.screenshots.map((src, index) => (
-          <Image
-            key={index}
-            src={src}
-            alt={`${project.title} screenshot ${index + 1}`}
-            width={500}
-            height={300}
-            className="rounded-lg"
-          />
+        {project.screenshots.map((x, index) => (
+          <div className="flex flex-col items-center" key={index}>
+            <Image
+              key={index}
+              src={x.src}
+              alt={`${project.title} screenshot ${index + 1}`}
+              width={x.width ?? 500}
+              height={x.height ?? 300}
+              className="rounded-lg"
+            />
+            {x.desc && <p className="text-sm dark:text-gray-100">{x.desc}</p>}
+          </div>
         ))}
       </div>
     </div>
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,32 +3,35 @@ export const projects = [
     id: 1,
     title: 'OpenAI Chat',
     description:
-      'Developed a full-stack OpenAI Chat client for employees. Includes an Admin screen manage individual Bots. Built front-end with Next.js, back-end with C#/.NET Core, and MS SQL for data storage.',
+      'A full-stack OpenAI Chat client for employees. Includes an Admin screen manage individual Bots. Built front-end with Next.js, back-end with C#/.NET Core, and MS SQL for data storage.',
     technologies: ['React.js', 'Next.js', 'C#', '.NET Core', 'MS SQL'],
     screenshots: [
-      '/images/aichat1.png',
-      '/images/aichat2.png',
-      '/images/aichat3.png',
+      { src: '/images/aichat1.png', desc: 'AI Chat Interface' },
+      { src: '/images/aichat2.png', desc: 'Admin page #1 - List of configured bots' },
+      { src: '/images/aichat3.png', desc: 'Admin page #2 - Edit Bot' },
     ],
   },
   {
     id: 3,
     title: `Logo Configuration`,
     description:
-      'Developed a unique modal dialog, allowing custom features to be configured in a visually appealing way. Allows images to be uploaded and custom colors to be set.',
+      'A unique modal dialog, allowing custom features to be configured in a visually appealing way. Allows images to be uploaded and custom colors to be set.',
     technologies: ['React.js', 'Next.js', 'Chakra-UI'],
     screenshots: [
-      '/images/logocfg1.png',
-      '/images/logocfg2.png',
+      { src: '/images/logocfg1.png', desc: 'Edit Configuration' },
+      { src: '/images/logocfg2.png', desc: 'Edit Configuration - Custom Colors', width: 200 },
     ],
   },
   {
     id: 3,
     title: `Custom 'Select' Component`,
     description:
-      'Developed a custom react component to replace react-select. Allows single/multi-select, custom rendering, filters, and infinity scrolling. Built front-end with React.js and Chakra-UI, back-end with C#/.NET Core (to support filtering/infinity scrolling).',
+      'A custom react component to replace react-select. Allows single/multi-select, custom rendering, filters, and infinity scrolling. Built front-end with React.js and Chakra-UI, back-end with C#/.NET Core (to support filtering/infinity scrolling).',
     technologies: ['React.js', 'Next.js', 'Chakra-UI', 'C#', '.NET Core'],
-    screenshots: ['/images/worf1.png', '/images/worf2.png'],
+    screenshots: [
+      { src: '/images/worf1.png', desc: 'Test Page - All Variants' },
+      { src: '/images/worf2.png', desc: 'Multi-select + Infinity scrolling', width: 200 },
+    ],
   },
   // Add more projects
 ];
